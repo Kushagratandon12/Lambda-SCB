4e2a623106b926b2b22d8cc0306ec467ba0d53d6
kushagratandon12 <kushagra.tandon@yahoo.com>
2022-06-29 09:48:54 +0530

    update: testing

---
+++ testing/app.py
@@ -0,0 +1,25 @@
+from re import T
+import requests
+import subprocess
+def pinging_py():
+    while True:
+        url = "https://lambda-smartbankapp.api.cloudtech-training.com"
+        payload={}
+        headers = {}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        # print(response.text)
+
+def pinging_2():
+    while True:
+        res = subprocess.Popen(['curl' , '--location' , '--request' , 'GET' , 'https://lambda-smartbankapp.api.cloudtech-training.com'])
+        if res == 0:
+            print("Ping successfully done")
+        elif res ==2:
+            print("No Respoonse from address")
+        else:
+            print("Failed")
+
+
+
+# if __name__ == "__main__":
+pinging_2()
